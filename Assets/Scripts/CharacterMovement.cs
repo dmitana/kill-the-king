82d5114e84678d18886d3502f17e9c375a417b3e
Denis Mitana <denis.mitana1@gmail.com>
2020-11-04 21:31:19 +0100

    Fix vertical movement

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -6,16 +6,14 @@ public class CharacterMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float speed = 1;
-    
-    // Start is called before the first frame update
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed;
+        rb.velocity = new Vector3(Input.GetAxis("Horizontal"), 0.0f) * speed;
     }
 }
