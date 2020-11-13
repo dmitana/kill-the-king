d357be90796ac1e016ac4b6d2d4d29649c7727a8
Denis Mitana <denis.mitana1@gmail.com>
2020-11-13 18:34:33 +0100

    Update character's skills

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -2,26 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character : MonoBehaviour
-{
+public class Character : MonoBehaviour {
+	public string characterName;
     public int health;
     public int baseStrength;
-    public List<Skill> skills;
-    
-    private List<Effect> _activeEffects;
-    // Start is called before the first frame update
-    void Start()
-    {
-        skills = new List<Skill>();
-        _activeEffects = new List<Effect>();
-    }
 
-    public void AddEffect(Effect effect)
-    {
+	public List<Skill> availableSkills = new List<Skill>();
+    public List<Skill> skills = new List<Skill>();
+
+    private List<Effect> _activeEffects = new List<Effect>();
+
+    public void AddEffect(Effect effect) {
         _activeEffects.Add(effect);
     }
 
     public void DecreaseHealth(int damage) {
         health -= damage;
     }
+
+	public void AddSkill(Skill skill) {
+		if (!availableSkills.Contains(skill))
+            throw new System.ArgumentException(
+				$"Trying to add {skill.name}, which is not available for current character."
+			);
+
+		skills.Add(skill);
+	}
+
+	public void RemoveSkill(Skill skill) {
+		skills.Remove(skill);
+	}
 }
