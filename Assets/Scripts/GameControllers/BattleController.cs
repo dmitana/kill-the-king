d24220cb658d2e6139c93056ccf923534dd4ba7f
Denis Mitana <denis.mitana1@gmail.com>
2020-12-07 18:19:23 +0100

    Refactor BattleController

--- Assets/Scripts/GameControllers/BattleController.cs
+++ Assets/Scripts/GameControllers/BattleController.cs
@@ -5,83 +5,77 @@ using UnityEngine;
 using Random = System.Random;
 
 public class BattleController : MonoBehaviour {
-    private List<Character> enemies;
     public List<Character> Enemies { get; set; }
-    private GameObject playerTeam;
-    private GameObject enemyTeam;
     public Character ChosenCharacter { get; set; }
     public List<Character> ChosenTargets { get; private set; }
-
     public List<Character> ValidTargets { get; private set; }
     public Skill ChosenSkill { get; set; }
 
+    private Team playerTeam;
+    private Team enemyTeam;
+	private SceneController sceneController;
     private Team currentTeam;
     private bool playerTeamFirst;
     private GameObject skillField;
-
     private Random rng;
 
     private void Awake() {
         rng = new Random();
     }
 
-    public void FillEnemiesToBattle() {
-        enemyTeam = GameObject.FindGameObjectWithTag("EnemyTeam");
-        Team enemyTeamComp = enemyTeam.GetComponent<Team>();
-        Transform enemyTeamTransform = enemyTeam.transform;
-        Vector3 position = enemyTeamTransform.position;
+    private void FillEnemiesToBattle() {
+        Vector3 position = enemyTeam.transform.position;
 
         foreach (Character enemy in Enemies) {
-            Character newEnemy = Instantiate(enemy, position, Quaternion.identity, enemyTeamTransform);
-            enemyTeamComp.AddCharacterToTeam(newEnemy);
+            Character newEnemy = Instantiate(enemy, position, Quaternion.identity, enemyTeam.transform);
+            enemyTeam.AddCharacterToTeam(newEnemy);
             position[0] += 1;
         }
     }
 
-    public void InitializeCombat() {
-        playerTeam = GameObject.FindGameObjectWithTag("PlayerTeam");
+    public void InitializeBattle() {
+		playerTeam = Team.playerTeamInstance;
         playerTeam.transform.position = new Vector3(-5, 0, 0);
-        playerTeam.GetComponent<CharacterMovement>().enabled = false;
-
         playerTeamFirst = (rng.Next(2) == 0);
 
-        Team playerTeamComp = (Team) playerTeam.GetComponent(typeof(Team));
-		playerTeamComp.InitilizeForBattle(this);
+        enemyTeam = GameObject.FindGameObjectWithTag("EnemyTeam").GetComponent<Team>();
+		FillEnemiesToBattle();
 
-        Team enemyTeamComp = (Team) enemyTeam.GetComponent(typeof(Team));
-		enemyTeamComp.InitilizeForBattle(this);
+		playerTeam.InitilizeForBattle(this);
+		enemyTeam.InitilizeForBattle(this);
 
         skillField = GameObject.FindGameObjectWithTag("SkillField");
         foreach (UISkillField field in skillField.transform.GetComponentsInChildren<UISkillField>()) {
             field.SetBattleController(this);
         }
 
-        StartCoroutine(Combat());
+        StartCoroutine(Battle());
     }
 
     private IEnumerator Combat() {
         Team playerTeamComp = (Team) playerTeam.GetComponent(typeof(Team));
         Team enemyTeamComp = (Team) enemyTeam.GetComponent(typeof(Team));
 
-        while (playerTeamComp.Characters.Count > 0 && enemyTeamComp.Characters.Count > 0) {
+    private IEnumerator Battle() {
+        while (playerTeam.Characters.Count > 0 && enemyTeam.Characters.Count > 0) {
             Debug.Log("Nove kolo zacalo");
-            foreach (Character c in playerTeamComp.Characters) {
-                playerTeamComp.AddUnplayedCharacter(c);
-                playerTeamComp.RemovePlayedCharacter(c); // prerobit tak, aby to vymazalo aj mrtve postavy
+            foreach (Character c in playerTeam.Characters) {
+                playerTeam.AddUnplayedCharacter(c);
+                playerTeam.RemovePlayedCharacter(c); // prerobit tak, aby to vymazalo aj mrtve postavy
             }
 
-            foreach (Character c in enemyTeamComp.Characters) {
-                enemyTeamComp.AddUnplayedCharacter(c);
-                enemyTeamComp.RemovePlayedCharacter(c);
+            foreach (Character c in enemyTeam.Characters) {
+                enemyTeam.AddUnplayedCharacter(c);
+                enemyTeam.RemovePlayedCharacter(c);
             }
 
             bool playerTeamRound = playerTeamFirst;
-            while (playerTeamComp.UnplayedCharacters.Count > 0 ||
-                   enemyTeamComp.UnplayedCharacters.Count > 0) {
+            while (playerTeam.UnplayedCharacters.Count > 0 ||
+                   enemyTeam.UnplayedCharacters.Count > 0) {
                 Debug.Log("Novy tah");
                 ChosenCharacter = null;
 
-                currentTeam = (playerTeamRound) ? playerTeamComp : enemyTeamComp;
+                currentTeam = (playerTeamRound) ? playerTeam: enemyTeam;
                 if (currentTeam.UnplayedCharacters.Count == 0) {
                     playerTeamRound = !playerTeamRound;
                     continue;
@@ -103,12 +97,12 @@ public class BattleController : MonoBehaviour {
                 }
 
                 ChosenTargets = new List<Character>();
-                ValidTargets = ChosenSkill.HighlightTargets(playerTeamComp, enemyTeamComp, playerTeamRound);
-                
+                ValidTargets = ChosenSkill.HighlightTargets(playerTeam, enemyTeam, playerTeamRound);
+
                 int numOfTargets = ChosenSkill.GetNumOfTargets();
                 if (numOfTargets == -1)
                     numOfTargets = ValidTargets.Count;
-                
+
                 if (currentTeam.isAI)
                     currentTeam.SelectTargets(numOfTargets, ValidTargets);
                 while (numOfTargets != ChosenTargets.Count) {
