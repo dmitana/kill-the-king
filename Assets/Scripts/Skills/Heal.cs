bdc6ede76f479d26e0c12e14ba8125ce62c13986
Denis Mitana <denis.mitana1@gmail.com>
2020-12-12 17:41:01 +0100

    Add Heal skill

---
+++ Assets/Scripts/Skills/Heal.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heal : Skill {
+    public override void ApplySkill(Character attacker, Character target) {
+    }
+
+    public override List<Character> HighlightTargets(Team playerTeam, Team enemyTeam, bool playerTeamTurn) {
+        throw new System.NotImplementedException();
+    }
+}
