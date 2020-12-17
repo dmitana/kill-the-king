14dcaba2964c0eb2dc522f27b35f6752a805c921
Miroslav Sumega <miro.sumega@gmail.com>
2020-12-17 12:26:15 +0100

    Add implementation and prefab settings of Area Attack

--- Assets/Scripts/Skills/AreaAttack.cs
+++ Assets/Scripts/Skills/AreaAttack.cs
@@ -1,11 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AreaAttack : Skill
 {
-    public override void ApplySkill(Character attacker, Character target) { }
+    public override void ApplySkill(Character attacker, Character target) {
+        var damage = (int) Math.Round(attacker.baseStrength * strength);
+        target.DecreaseHealth(damage);
+    }
+    
     public override List<Character> HighlightTargets(Team playerTeam, Team enemyTeam, bool playerTeamTurn) {
-        throw new System.NotImplementedException();
+        battleController.Log = $"Number of targets: {numOfTargets}";
+        battleController.Log = "Only enemies are valid targets.";
+        return (playerTeamTurn) ? enemyTeam.Characters : playerTeam.Characters;
     }
 }
