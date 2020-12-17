de3e7e4d357a203bb714c402db89d46ef4e9a094
Denis Mitana <denis.mitana1@gmail.com>
2020-12-17 14:29:59 +0100

    Add RobberyOfTrader event

---
+++ Assets/Scripts/Events/RobberyOfTrader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobberyOfTrader : Event {
+	public override void OnAccept() {
+		if (rnd.NextDouble() < 0.5f) {
+			Success();
+		}
+		else {
+			GenerateEnemies();
+			MoveToBattle();
+		}
+	}
+}
