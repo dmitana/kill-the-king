d72004f3c4929370927d6ec1b54512d99df3ff31
Denis Mitana <denis.mitana1@gmail.com>
2020-12-17 13:14:05 +0100

    Add PeasantInDanger event

--- Assets/Scripts/Events/PeasantInDanger.cs
+++ Assets/Scripts/Events/PeasantInDanger.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PeasantInDanger : Event {
+	protected override void Initialize() {
+		int idxToRemove = rnd.Next(0, possibleEnemies.Count);
+		possibleEnemies.RemoveAt(idxToRemove);
+		GenerateEnemies();
+	}
+
     public override void OnAccept() {
 		MoveToBattle(false);
     }
