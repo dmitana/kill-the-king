a6a85111bec26010d8f5b50f3a32bbe4f3552f29
Denis Mitana <denis.mitana1@gmail.com>
2020-12-17 20:36:24 +0100

    Add MessageWindowUI

---
+++ Assets/Scripts/UI/MessageWindowUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessageWindowUI : MonoBehaviour {
+	public Image messageWindowUI;
+	public TMP_Text messageText;
+	public int hideAfterSec = 5;
+
+	private List<Event> events;
+
+	void OnEnable() {
+		events = new List<Event>();
+		GameObject[] eventsGO = GameObject.FindGameObjectsWithTag("Event");
+
+		foreach (GameObject go in eventsGO) {
+			Event e = go.GetComponent<Event>();
+			e.onFinish += Show;
+			events.Add(e);
+		}
+	}
+
+	private void Show(Event e) {
+		messageWindowUI.gameObject.SetActive(true);
+		messageText.text = e.Message;
+		StartCoroutine(Hide());
+	}
+
+	 IEnumerator Hide() {
+         yield return new WaitForSeconds(hideAfterSec);
+         messageWindowUI.gameObject.SetActive(false);
+     }
+}
