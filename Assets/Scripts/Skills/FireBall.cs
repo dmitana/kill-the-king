0afade074c5093164be7aeaa6857b0867b9b7fa7
Denis Mitana <denis.mitana1@gmail.com>
2020-12-18 18:55:15 +0100

    Add Fire Ball skill

--- Assets/Scripts/Skills/FireBall.cs
+++ Assets/Scripts/Skills/FireBall.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FireBall : Skill
-{
-    public override void ApplySkill(Character attacker, Character target) { }
+public class FireBall : Skill {
+    public override void ApplySkill(Character attacker, Character target) {
+		int damage = attacker.baseStrength;
+        target.DecreaseHealth(damage);
+        if (rnd.NextDouble() < strength) {
+            Effect effect = Instantiate(effects[0], target.gameObject.transform);
+            effect.Strength = attacker.baseStrength;
+            target.AddEffect(effect);
+        }
+	}
     public override List<Character> HighlightTargets(Team playerTeam, Team enemyTeam, bool playerTeamTurn) {
-        throw new System.NotImplementedException();
+        battleController.Log = $"Number of targets: {numOfTargets}";
+        battleController.Log = "Only enemies are valid targets.";
+        return (playerTeamTurn) ? enemyTeam.Characters : playerTeam.Characters;
     }
 }
