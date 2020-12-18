1bcdc2713829c211744b73482097c294652992e6
Denis Mitana <denis.mitana1@gmail.com>
2020-12-18 18:54:29 +0100

    Rename Bleeding effect to DamageOverTime

--- Assets/Scripts/Effects/DamageOverTime.cs
+++ Assets/Scripts/Effects/DamageOverTime.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bleeding : Effect {
+public class DamageOverTime : Effect {
     public double damagePerRound;
 
     public override void AtRoundEnd(Character c) {
