45cf2410661f03cc4a8e3876be2b9b2123b0ca47
Denis Mitana <denis.mitana1@gmail.com>
2020-12-18 20:02:35 +0100

    Add Poisonous Attack skill

--- Assets/Scripts/Skills/PoisonousAttack.cs
+++ Assets/Scripts/Skills/PoisonousAttack.cs
@@ -3,8 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PoisonousAttack : Skill {
-    public override void ApplySkill(Character attacker, Character target) { }
+    public override void ApplySkill(Character attacker, Character target) {
+		int damage = attacker.baseStrength / 2;
+        target.DecreaseHealth(damage);
+		DamageOverTime effect = (DamageOverTime) Instantiate(effects[0], target.gameObject.transform);
+		effect.Strength = attacker.baseStrength;
+		effect.damagePerRound = strength;
+		target.AddEffect(effect);
+	}
+
     public override List<Character> HighlightTargets(Team playerTeam, Team enemyTeam, bool playerTeamTurn) {
-        throw new System.NotImplementedException();
+        battleController.Log = $"Number of targets: {numOfTargets}";
+        battleController.Log = "Only enemies are valid targets.";
+        return (playerTeamTurn) ? enemyTeam.Characters : playerTeam.Characters;
     }
 }
