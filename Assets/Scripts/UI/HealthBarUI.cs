7cb2ee6aca1b5777f4e834c98180637c96216c37
Denis Mitana <denis.mitana1@gmail.com>
2020-12-20 18:16:24 +0100

    Add comments to the health bar UI

--- Assets/Scripts/UI/HealthBarUI.cs
+++ Assets/Scripts/UI/HealthBarUI.cs
@@ -4,12 +4,18 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// Controls health bar UI displaying character's health.
+/// </summary>
 public class HealthBarUI : MonoBehaviour {
 	public Character character;
 	[Space]
 	public Slider healthBar;
 	public TMP_Text healthText;
 
+	/// <summary>
+	/// Displays character's health only in a battle.
+	/// </summary>
 	void Update() {
 		if (character.InBattle) {
 			healthBar.gameObject.SetActive(true);
